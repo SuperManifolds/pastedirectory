2e67922665bb047267f26f5e13a7696af92ef1bb
Alex Sørlie Glomsaas <alex@delexious.com>
2014-04-30 20:56:37 +0200

    Added some error page templates.

--- error_handlers.py
+++ error_handlers.py
@@ -6,6 +6,11 @@ from flask import Blueprint, render_template, abort
 error_handlers = Blueprint('error_handlers', __name__,
                         template_folder='templates')
 
+
+@error_handlers.app_errorhandler(400)
+def page_not_found(error):
+	return render_template('errors/400.html'), 400
+
 @error_handlers.app_errorhandler(401)
 def page_not_found(error):
 	return render_template('errors/401.html'), 401
@@ -17,6 +22,42 @@ def page_not_found(error):
 @error_handlers.app_errorhandler(404)
 def page_not_found(error):
 	return render_template('errors/404.html'), 404
+
+@error_handlers.app_errorhandler(405)
+def page_not_found(error):
+	return render_template('errors/405.html'), 405
+
+@error_handlers.app_errorhandler(406)
+def page_not_found(error):
+	return render_template('errors/406.html'), 406
+
+@error_handlers.app_errorhandler(408)
+def page_not_found(error):
+	return render_template('errors/408.html'), 408
+
+@error_handlers.app_errorhandler(409)
+def page_not_found(error):
+	return render_template('errors/409.html'), 409
+
+@error_handlers.app_errorhandler(410)
+def page_not_found(error):
+	return render_template('errors/410.html'), 410
+
+@error_handlers.app_errorhandler(411)
+def page_not_found(error):
+	return render_template('errors/411.html'), 411
+
+@error_handlers.app_errorhandler(412)
+def page_not_found(error):
+	return render_template('errors/412.html'), 412
+
+@error_handlers.app_errorhandler(413)
+def page_not_found(error):
+	return render_template('errors/413.html'), 413
+
+@error_handlers.app_errorhandler(414)
+def page_not_found(error):
+	return render_template('errors/414.html'), 414
 
 @error_handlers.app_errorhandler(415)
 def page_not_found(error):
