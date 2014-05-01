f3a77b41eaeaa2e76a4e8bf4515bd5911d1fa72d
Alex Sørlie Glomsaas <alex@delexious.com>
2014-05-01 04:21:18 +0200

    Add shell script for importing language database

---
+++ importlanguages.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+read -p "Are you sure you want to overwrite the language database y/n? " prompt
+    case "$prompt" in
+            y*)  	while getopts "d" opt
+					do
+						case $opt in
+							d)
+								printf "\n\nDownloading latest database version\n\n"
+								wget https://paste.directory/admin/exportlanguages -N -O languages.json
+								;;
+						esac
+					done
+					printf "\n\nOverwriting database\n\n"
+					mongoimport -d "pastedirectory" -c "languages"  --drop --file languages.json
+					printf "\n\nOperation completed\n\n"
+					;;
+            n*)  echo "Operation aborted" ;;
+    esac
+	
