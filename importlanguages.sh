dd104902066b937e4d526b908212fdb5a80b831d
foldericon <foldericon@gmail.com>
2014-05-05 21:40:49 +0200

    Revert "Fix importlanguages.sh"
    This reverts commit 9cf729b16da1d4147e98884340c874b0d78023b4.
    
    Conflicts:
    	importlanguages.sh

--- importlanguages.sh
+++ importlanguages.sh
@@ -7,18 +7,14 @@ read -p "Are you sure you want to overwrite the language database y/n? " prompt
 						case $opt in
 							d)
 								printf "\n\nDownloading latest database version\n\n"
-								wget https://paste.directory/admin/exportlanguages -N -O languages.json --no-check-certificate
-								if [[ $? > 0 ]]; then
-									printf "\n\nError while retrieving the file, aborting.\n\n"
-									exit 1
-								fi
+								wget https://paste.directory/admin/exportlanguages -N -O languages.json
 								;;
 						esac
 					done
-						printf "\n\nOverwriting database\n\n"
-						mongoimport -d "pastedirectory" -c "languages"  --drop --file languages.json
-						printf "\n\nOperation completed\n\n"
-						;;
+					printf "\n\nOverwriting database\n\n"
+					mongoimport -d "pastedirectory" -c "languages"  --drop --file languages.json
+					printf "\n\nOperation completed\n\n"
+					;;
             n*)  echo "Operation aborted" ;;
     esac
 	
