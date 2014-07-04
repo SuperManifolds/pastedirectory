927210eb51977b3ff5d09d03bfd7de467d744fe8
Alex Sørlie Glomsaas <alex@delexious.com>
2014-07-04 12:29:33 +0200

    Use curl instead of wget for language updates

--- importlanguages.sh
+++ importlanguages.sh
@@ -7,11 +7,11 @@ read -p "Are you sure you want to overwrite the language database y/n? " prompt
 						case $opt in
 							d)
 								printf "\n\nDownloading latest database version\n\n"
-								wget https://paste.directory/admin/exportlanguages -N -O languages.json --no-check-certificate
+                                curl -kso languages.json https://paste.directory/admin/exportlanguages
 								if [[ $? > 0 ]]; then
 									printf "\n\nError while retrieving the file, aborting.\n\n"
-									exit 1 
-								fi	
+									exit 1
+								fi
 								;;
 						esac
 					done
@@ -21,4 +21,3 @@ read -p "Are you sure you want to overwrite the language database y/n? " prompt
 					;;
             n*)  echo "Operation aborted" ;;
     esac
-	
